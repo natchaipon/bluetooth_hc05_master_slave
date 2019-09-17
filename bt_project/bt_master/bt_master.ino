e4f940d59e1f6111f1e792927010aba343729e73
Natchaipon <peepee.popoz@gmail.com>
2019-09-17 15:43:49 +0700

    Add files via upload

---
+++ bt_project/bt_master/bt_master.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); 
+
+
+int state = 0;
+int potValue = 0;
+void setup() {
+
+  mySerial.begin(38400); 
+  Serial.begin(9600);
+}
+void loop() {
+  if (Serial.available() > 0) { 
+    state = Serial.read(); 
+  }
+
+  potValue = analogRead(A0);
+  Serial.println(potValue);
+  int potValueMapped = map(potValue, 0, 1023, 0, 160);
+  mySerial.write(potValueMapped); 
+  delay(10);
+}
---
+++ bt_project/bt_slave/bt_slave.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+SoftwareSerial mySerial(10, 11); 
+Servo myServo;
+int state = 20;
+
+void setup() {
+  myServo.attach(9);
+  Serial.begin(9600); 
+  mySerial.begin(38400);
+}
+
+void loop() {
+  
+ if(mySerial.available() > 0){ 
+    state = mySerial.read(); 
+    Serial.println(state);
+ }
+
+ myServo.write(state);
+ delay(10);
+
+}
